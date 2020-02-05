2b9154aa45191db473bb9646c29b050efefa08b3
Arthur923 <54887865+Arthur923@users.noreply.github.com>
2020-02-05 14:51:42 +0200

    Add files via upload

---
+++ var.js
@@ -0,0 +1,12 @@
+var text = 'wwDbcvqwC';
+
+//  , upper = text.match(/[A-Z]/g).length
+//  , lower = text.match(/[a-z]/g).length
+//  ;
+
+function count(arg){
+    console.log(arg.lenght);
+    alert(arg.lenght);
+}
+
+count(text)
